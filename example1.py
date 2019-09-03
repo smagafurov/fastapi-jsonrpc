b7b34dc8dcf7f15c956c9e7a3b99a4c52a3f4c57
Sergey Magafurov <magafurov@tochka.com>
2019-09-03 16:02:01 +0500

    pydantic 0.32.2, fastapi = 0.38.1

---
+++ example1.py
@@ -0,0 +1,34 @@
+import fastapi_jsonrpc as jsonrpc
+from pydantic import BaseModel
+from fastapi import Body
+
+
+app = jsonrpc.API()
+
+api_v1 = jsonrpc.Entrypoint('/api/v1/jsonrpc')
+
+
+class MyError(jsonrpc.BaseError):
+    CODE = 5000
+    MESSAGE = 'My error'
+
+    class DataModel(BaseModel):
+        details: str
+
+
+@api_v1.method(errors=[MyError])
+def echo(
+    data: str = Body(..., example='123'),
+) -> str:
+    if data == 'error':
+        raise MyError(data={'details': 'error'})
+    else:
+        return data
+
+
+app.bind_entrypoint(api_v1)
+
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run('example1:app', port=5000, debug=True, access_log=False)
