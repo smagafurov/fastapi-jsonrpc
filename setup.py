256f1c80884d9d254ea9502b674f9b0928cc095a
Sergey Magafurov <magafurov@tochka.com>
2020-08-11 15:32:40 +0500

    Bump version 0.2.5

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='fastapi-jsonrpc',
-    version='0.2.4',
+    version='0.2.5',
     description='JSON-RPC server based on fastapi',
     python_requires='==3.*,>=3.6.0',
     project_urls={'homepage': 'https://github.com/smagafurov/fastapi-jsonrpc', 'repository': 'https://github.com/smagafurov/fastapi-jsonrpc'},
@@ -34,6 +34,6 @@ setup(
     keywords='json-rpc asgi swagger openapi fastapi pydantic starlette',
     packages=['fastapi_jsonrpc'],
     package_data={},
-    install_requires=['aiojobs==0.*,>=0.2.2', 'fastapi==0.*,>=0.55.1', 'pydantic==1.*,>=1.5.1', 'starlette==0.*,>=0.13.2'],
+    install_requires=['aiojobs==0.*,>=0.2.2', 'fastapi==0.*,>=0.55.0', 'pydantic==1.*,>=1.5.1', 'starlette==0.*,>=0.13.2'],
     extras_require={'dev': ['pygments==2.*,>=2.4.0', 'pytest==5.*,>=5.2.2', 'rst-include==1.*,>=1.0.0', 'uvicorn==0.*,>=0.8.6']},
 )
