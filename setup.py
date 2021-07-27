2f6e76e571b7a970ecf94e52453f888ee4e6e548
Sergey Magafurov <magafurov@tochka.com>
2021-07-27 11:12:53 +0500

    chore: Bump version 2.1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ if os.path.exists(readme_path):
 setup(
     long_description=readme,
     name='fastapi-jsonrpc',
-    version='2.1.1',
+    version='2.1.2',
     description='JSON-RPC server based on fastapi',
     python_requires='==3.*,>=3.6.0',
     project_urls={'homepage': 'https://github.com/smagafurov/fastapi-jsonrpc', 'repository': 'https://github.com/smagafurov/fastapi-jsonrpc'},
@@ -35,5 +35,5 @@ setup(
     packages=['fastapi_jsonrpc'],
     package_data={},
     install_requires=['aiojobs==0.*,>=0.2.2', 'fastapi>0.55'],
-    extras_require={'dev': ['pygments==2.*,>=2.4.0', 'pytest==5.*,>=5.2.2', 'rst-include==2.*,>=2.1.0', 'six==1.*,>=1.15.0', 'uvicorn==0.*,>=0.8.6']},
+    extras_require={'dev': ['pygments==2.*,>=2.4.0', 'pytest==6.*,>=6.2.0', 'requests==2.*,>=2.26.0', 'rst-include==2.*,>=2.1.0', 'sentry-sdk==1.*,>=1.3.0', 'six==1.*,>=1.15.0', 'uvicorn==0.*,>=0.14.0']},
 )
