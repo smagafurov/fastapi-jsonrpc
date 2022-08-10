e96ae97383b391e369712559fd0a3e8c41d0cfe0
Sergey Magafurov <magafurov@tochka.com>
2022-08-10 23:44:41 +0500

    test: fix order dependency

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,3 +1,5 @@
+from unittest.mock import ANY
+
 import pytest
 from fastapi import Depends, Body, Header
 from typing import Tuple
@@ -19,10 +21,10 @@ def ep(ep_path):
 
     def get_common_counter(
         common: str = Body(...),
-    ) -> str:
+    ) -> Tuple[str, int]:
         nonlocal _common_counter
         _common_counter += 1
-        return f'{common}-{_common_counter}'
+        return common, _common_counter
 
     ep = jsonrpc.Entrypoint(
         ep_path,
@@ -33,9 +35,9 @@ def ep(ep_path):
     @ep.method()
     def probe(
         shared_counter: str = Depends(get_shared_counter),
-        common_counter: str = Depends(get_common_counter),
-    ) -> Tuple[str, str]:
-        return shared_counter, common_counter
+        common_counter: Tuple[str, int] = Depends(get_common_counter),
+    ) -> Tuple[str, str, int]:
+        return shared_counter, common_counter[0], common_counter[1]
 
     return ep
 
@@ -45,9 +47,9 @@ def test_single(method_request):
     resp2 = method_request('probe', {'common': 'two'}, request_id=222)
     resp3 = method_request('probe', {'common': 'three'}, request_id=333)
     assert [resp1, resp2, resp3] == [
-        {'id': 111, 'jsonrpc': '2.0', 'result': ['shared-1', 'one-1']},
-        {'id': 222, 'jsonrpc': '2.0', 'result': ['shared-2', 'two-2']},
-        {'id': 333, 'jsonrpc': '2.0', 'result': ['shared-3', 'three-3']},
+        {'id': 111, 'jsonrpc': '2.0', 'result': ['shared-1', 'one', 1]},
+        {'id': 222, 'jsonrpc': '2.0', 'result': ['shared-2', 'two', 2]},
+        {'id': 333, 'jsonrpc': '2.0', 'result': ['shared-3', 'three', 3]},
     ]
 
 
@@ -73,7 +75,8 @@ def test_batch(json_request):
         },
     ])
     assert resp == [
-        {'id': 111, 'jsonrpc': '2.0', 'result': ['shared-1', 'one-1']},
-        {'id': 222, 'jsonrpc': '2.0', 'result': ['shared-1', 'two-2']},
-        {'id': 333, 'jsonrpc': '2.0', 'result': ['shared-1', 'three-3']},
+        {'id': 111, 'jsonrpc': '2.0', 'result': ['shared-1', 'one', ANY]},
+        {'id': 222, 'jsonrpc': '2.0', 'result': ['shared-1', 'two', ANY]},
+        {'id': 333, 'jsonrpc': '2.0', 'result': ['shared-1', 'three', ANY]},
     ]
+    assert set(r['result'][2] for r in resp) == {1, 2, 3}
