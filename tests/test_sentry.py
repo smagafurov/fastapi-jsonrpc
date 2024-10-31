f45aa62c4eb082c067f55b255f3c6143f588203f
spumer <spumer-tm@yandex.ru>
2024-10-31 12:30:51 +0500

    test(sentry): now test works correctly with SentrySDK 2.x; do not test 1.x version, it's deprecated

--- tests/test_sentry.py
+++ tests/test_sentry.py
@@ -5,8 +5,7 @@ TODO: move integration to sentry_sdk
 import pytest
 import sentry_sdk
 from sentry_sdk import Transport
-
-from sentry_sdk.utils import capture_internal_exceptions
+from sentry_sdk.envelope import Envelope
 
 
 @pytest.fixture
@@ -62,50 +61,21 @@ def test_transaction_is_jsonrpc_method(
     ]) == {'test_sentry.probe.<locals>.probe', 'test_sentry.probe.<locals>.probe2'}
 
 
-class _TestTransport(Transport):
-    def __init__(self, capture_event_callback, capture_envelope_callback):
+class TestTransport(Transport):
+    def __init__(self):
         Transport.__init__(self)
-        self.capture_event = capture_event_callback
-        self.capture_envelope = capture_envelope_callback
-        self._queue = None
+
+    def capture_envelope(self, _: Envelope) -> None:
+        """No-op capture_envelope for tests"""
+        pass
 
 
 @pytest.fixture
-def monkeypatch_test_transport(monkeypatch):
-    def check_event(event):
-        def check_string_keys(map):
-            for key, value in map.items:
-                assert isinstance(key, str)
-                if isinstance(value, dict):
-                    check_string_keys(value)
-
-        with capture_internal_exceptions():
-            check_string_keys(event)
-
-    def check_envelope(envelope):
-        with capture_internal_exceptions():
-            # Assert error events are sent without envelope to server, for compat.
-            # This does not apply if any item in the envelope is an attachment.
-            if not any(x.type == "attachment" for x in envelope.items):
-                assert not any(item.data_category == "error" for item in envelope.items)
-                assert not any(item.get_event() is not None for item in envelope.items)
-
-    def inner(client):
-        monkeypatch.setattr(
-            client, "transport", _TestTransport(check_event, check_envelope)
-        )
-
-    return inner
-
-
-@pytest.fixture
-def sentry_init(monkeypatch_test_transport, request):
+def sentry_init(request):
     def inner(*a, **kw):
-        hub = sentry_sdk.Hub.current
+        kw.setdefault("transport", TestTransport())
         client = sentry_sdk.Client(*a, **kw)
-        hub.bind_client(client)
-        if "transport" not in kw:
-            monkeypatch_test_transport(sentry_sdk.Hub.current.client)
+        sentry_sdk.get_global_scope().set_client(client)
 
     if request.node.get_closest_marker("forked"):
         # Do not run isolation if the test is already running in
@@ -113,49 +83,80 @@ def sentry_init(monkeypatch_test_transport, request):
         # fork)
         yield inner
     else:
-        with sentry_sdk.Hub(None):
+        old_client = sentry_sdk.get_global_scope().client
+        try:
+            sentry_sdk.get_current_scope().set_client(None)
             yield inner
+        finally:
+            sentry_sdk.get_global_scope().set_client(old_client)
 
 
 @pytest.fixture
 def capture_events(monkeypatch):
     def inner():
         events = []
-        test_client = sentry_sdk.Hub.current.client
-        old_capture_event = test_client.transport.capture_event
+        test_client = sentry_sdk.get_client()
         old_capture_envelope = test_client.transport.capture_envelope
 
-        def append_event(event):
-            events.append(event)
-            return old_capture_event(event)
-
-        def append_envelope(envelope):
+        def append_event(envelope):
             for item in envelope:
                 if item.headers.get("type") in ("event", "transaction"):
-                    test_client.transport.capture_event(item.payload.json)
+                    events.append(item.payload.json)
             return old_capture_envelope(envelope)
 
-        monkeypatch.setattr(test_client.transport, "capture_event", append_event)
-        monkeypatch.setattr(test_client.transport, "capture_envelope", append_envelope)
+        monkeypatch.setattr(test_client.transport, "capture_envelope", append_event)
+
         return events
 
     return inner
 
 
 @pytest.fixture
+def capture_envelopes(monkeypatch):
+    def inner():
+        envelopes = []
+        test_client = sentry_sdk.get_client()
+        old_capture_envelope = test_client.transport.capture_envelope
+
+        def append_envelope(envelope):
+            envelopes.append(envelope)
+            return old_capture_envelope(envelope)
+
+        monkeypatch.setattr(test_client.transport, "capture_envelope", append_envelope)
+
+        return envelopes
+
+    return inner
+
+
+
+@pytest.fixture
 def capture_exceptions(monkeypatch):
     def inner():
         errors = set()
-        old_capture_event = sentry_sdk.Hub.capture_event
+        old_capture_event_hub = sentry_sdk.Hub.capture_event
+        old_capture_event_scope = sentry_sdk.Scope.capture_event
 
-        def capture_event(self, event, hint=None):
+        def capture_event_hub(self, event, hint=None, scope=None):
+            """
+            Can be removed when we remove push_scope and the Hub from the SDK.
+            """
             if hint:
                 if "exc_info" in hint:
                     error = hint["exc_info"][1]
                     errors.add(error)
-            return old_capture_event(self, event, hint=hint)
+            return old_capture_event_hub(self, event, hint=hint, scope=scope)
 
-        monkeypatch.setattr(sentry_sdk.Hub, "capture_event", capture_event)
+        def capture_event_scope(self, event, hint=None, scope=None):
+            if hint:
+                if "exc_info" in hint:
+                    error = hint["exc_info"][1]
+                    errors.add(error)
+            return old_capture_event_scope(self, event, hint=hint, scope=scope)
+
+        monkeypatch.setattr(sentry_sdk.Hub, "capture_event", capture_event_hub)
+        monkeypatch.setattr(sentry_sdk.Scope, "capture_event", capture_event_scope)
+
         return errors
 
     return inner
